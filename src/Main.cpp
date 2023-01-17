049a6313d486e768992df649c286abf705be56aa
Andreas Wiesinger <awiesing90@gmail.com>
2023-01-17 12:57:08 +0100

    Initialize repo (#2)
    Provided code base for the tutorial, first version

---
+++ src/Main.cpp
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2023 TU Wien, Institute of Visual Computing & Human-Centered Technology.
+ */
+
+// Include our framework and the Vulkan headers:
+#include "VulkanLaunchpad.h"
+#include <vulkan/vulkan.h>
+
+// Include some local helper functions:
+#include "VulkanHelpers.h"
+#include "Teapot.h"
+
+/* ------------------------------------------------ */
+// Some more little helpers directly declared here:
+// (Definitions of functions below the main function)
+/* ------------------------------------------------ */
+
+/*!
+ *	This callback function gets invoked by GLFW whenever a GLFW error occured.
+ */
+void errorCallbackFromGlfw(int error, const char* description);
+
+/*!
+ *	Combines the arrays of const char* elements into a common vector of const char*.
+ *	@param		a			Pointer to the first element of array a.
+ *	@param		num_a		Number of array elements in a.
+ *	@param		a			Pointer to the first element of array b.
+ *	@param		num_b		Number of array elements in b.
+ *	@return 	A std::vector of const char* elements, containing all elements from a and b.
+ */ 
+std::vector<const char*> mergeArraysOfStrings(const char** a, uint32_t num_a, const char** b, uint32_t num_b);
+
+/*!
+ *	Based on the given physical device and the surface, select a queue family which supports both,
+ *	graphics and presentation to the given surface. Return the INDEX of an appropriate queue family!
+ *	@return		The index of a queue family which supports the required features shall be returned.
+ */
+uint32_t selectQueueFamilyIndex(VkPhysicalDevice physical_device, VkSurfaceKHR surface);
+
+/*!
+ *	Small helper macro that prepends a given path with "assets/shaders/".
+ *	@example 	auto pathToShaderFile = ADD_SHADER_DIR("my_shader.vert");
+ */
+#define ADD_SHADER_DIR(x) ("assets/shaders/" + std::string(x)).c_str()
+
+/* ------------------------------------------------ */
+// Main
+/* ------------------------------------------------ */
+
+int main(int argc, char** argv)
+{
+	// Some settings:
+	constexpr int window_width  = 800;
+	constexpr int window_height = 800;
+	constexpr bool fullscreen = false;
+	constexpr char* window_title = "An Introduction to Vulkan";
+
+	// Install a callback function, which gets invoked whenever a GLFW error occurred:
+	glfwSetErrorCallback(errorCallbackFromGlfw);
+
+	// Initialize GLFW:
+	if (!glfwInit()) {
+		VKL_EXIT_WITH_ERROR("Failed to init GLFW");
+	}
+
+	/* --------------------------------------------- */
+	// Task 1.1: Create a Window with GLFW
+	/* --------------------------------------------- */
+	// Use a monitor if we'd like to open the window in fullscreen mode:
+	GLFWmonitor* monitor = nullptr;
+	if (fullscreen) {
+		monitor = glfwGetPrimaryMonitor();
+	}
+
+	// Set some window settings before creating the window:
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // No need to create a graphics context for Vulkan
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow* window = nullptr;
+	
+	if (!window) {
+		VKL_EXIT_WITH_ERROR("No GLFW window created.");
+	}
+
+	/* --------------------------------------------- */
+	// Task 1.2: Create a Vulkan Instance
+	/* --------------------------------------------- */
+	VkInstance vk_instance = VK_NULL_HANDLE; // TODO: Set to a valid handle!
+
+	VkApplicationInfo application_info = {}; // Zero-initialize every member
+	application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; // Set this struct instance's type
+	application_info.pEngineName = "Vulkan Launchpad";
+	application_info.engineVersion = VK_MAKE_API_VERSION(0, 2023, 1, 0);
+	application_info.pApplicationName = "An Introduction to Vulkan";
+	application_info.applicationVersion = VK_MAKE_API_VERSION(0, 2023, 1, 1);
+	application_info.apiVersion = VK_API_VERSION_1_1;
+
+	VkInstanceCreateInfo instance_create_info = {}; // Zero-initialize every member
+	instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO; // Set this struct instance's type
+	instance_create_info.pApplicationInfo = &application_info;
+	
+	VkResult result = VK_ERROR_INITIALIZATION_FAILED;
+	VKL_CHECK_VULKAN_RESULT(result);
+
+	if (!vk_instance) {
+		VKL_EXIT_WITH_ERROR("No VkInstance created or handle not assigned.");
+	}
+	
+	/* --------------------------------------------- */
+	// Task 1.3: Create a Vulkan Window Surface
+	/* --------------------------------------------- */
+	VkSurfaceKHR vk_surface = VK_NULL_HANDLE; // TODO: Set to a valid handle!
+
+	if (!vk_surface) {
+		VKL_EXIT_WITH_ERROR("No VkSurfaceKHR created or handle not assigned.");
+	}
+
+	/* --------------------------------------------- */
+	// Task 1.4 Pick a Physical Device
+	/* --------------------------------------------- */
+	VkPhysicalDevice vk_physical_device = VK_NULL_HANDLE; // TODO: Set to a valid handle!
+	
+	if (!vk_physical_device) {
+		VKL_EXIT_WITH_ERROR("No VkPhysicalDevice selected or handle not assigned.");
+	}
+
+	/* --------------------------------------------- */
+	// Task 1.5: Select a Queue Family
+	/* --------------------------------------------- */
+
+	/* --------------------------------------------- */
+	// Task 1.6: Create a Logical Device and Get Queue
+	/* --------------------------------------------- */
+	VkDevice vk_device = VK_NULL_HANDLE; // TODO: Set to a valid handle!
+	VkQueue  vk_queue  = VK_NULL_HANDLE; // TODO: Set to a valid handle!
+
+	if (!vk_device) {
+		VKL_EXIT_WITH_ERROR("No VkDevice created or handle not assigned.");
+	}
+	if (!vk_queue) {
+		VKL_EXIT_WITH_ERROR("No VkQueue selected or handle not assigned.");
+	}
+
+	/* --------------------------------------------- */
+	// Task 1.7: Create Swap Chain
+	/* --------------------------------------------- */
+	VkSwapchainKHR vk_swapchain = VK_NULL_HANDLE; // TODO: Set to a valid handle!
+
+	VkSurfaceCapabilitiesKHR surface_capabilities = hlpGetPhysicalDeviceSurfaceCapabilities(vk_physical_device, vk_surface);
+	
+	// Build the swapchain config struct:
+	VkSwapchainCreateInfoKHR swapchain_create_info = {};
+	swapchain_create_info.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	swapchain_create_info.surface = vk_surface;
+	swapchain_create_info.minImageCount = surface_capabilities.minImageCount;
+	swapchain_create_info.imageArrayLayers = 1u;
+	swapchain_create_info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+	swapchain_create_info.preTransform = surface_capabilities.currentTransform;
+	swapchain_create_info.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	swapchain_create_info.clipped = VK_TRUE;
+	swapchain_create_info.queueFamilyIndexCount = 0;
+	swapchain_create_info.pQueueFamilyIndices = nullptr;
+
+	if (!vk_swapchain) {
+		VKL_EXIT_WITH_ERROR("No VkSwapchainKHR created or handle not assigned.");
+	}
+
+	/* --------------------------------------------- */
+	// Task 1.8: Initialize Vulkan Launchpad
+	/* --------------------------------------------- */
+
+	// Gather swapchain config as required by the framework:
+	VklSwapchainConfig swapchain_config = {};
+	swapchain_config.imageExtent = swapchain_create_info.imageExtent;
+	swapchain_config.swapchainHandle = vk_swapchain;
+	for (VkImage vk_image : swap_chain_images) {
+		VklSwapchainFramebufferComposition framebufferData;
+		// Fill the data for the color attachment:
+		framebufferData.colorAttachmentImageDetails.imageHandle = VK_NULL_HANDLE;
+		// Fill the data for the depth attachment:
+		framebufferData.depthAttachmentImageDetails.imageHandle = VK_NULL_HANDLE;
+		// Add it to the vector:
+		swapchain_config.swapchainImages.push_back(framebufferData);
+	}
+	
+	// Init the framework:
+	if (!vklInitFramework(vk_instance, vk_surface, vk_physical_device, vk_device, vk_queue, swapchain_config)) {
+		VKL_EXIT_WITH_ERROR("Failed to init Vulkan Launchpad");
+	}
+
+	// We set up a key callback via GLFW here:
+	glfwSetKeyCallback(window, [](GLFWwindow* glfw_window, int key, int scancode, int action, int mods) {
+		// We only listen to key release events (i.e., only key up, not key down):
+		if (action != GLFW_RELEASE) {
+			return;
+		}
+		// We mark the window that it should close if ESC is pressed:
+		if (key == GLFW_KEY_ESCAPE) { 
+			glfwSetWindowShouldClose(glfw_window, true); 
+		}
+	});
+
+	/* --------------------------------------------- */
+	// Task 1.9:  Implement the Render Loop
+	/* --------------------------------------------- */
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents(); // Handle user input
+		
+	}
+
+	// Wait for all GPU work to finish before cleaning up:
+	vkDeviceWaitIdle(vk_device);
+
+	/* --------------------------------------------- */
+	// Task 1.10: Cleanup
+	/* --------------------------------------------- */
+	vklDestroyFramework();
+
+	return EXIT_SUCCESS;
+}
+
+/* ------------------------------------------------ */
+// Definitions of little helpers defined above main:
+/* ------------------------------------------------ */
+
+void errorCallbackFromGlfw(int error, const char* description) {
+	std::cout << "GLFW error " << error << ": " << description << std::endl;
+}
+
+std::vector<const char*> mergeArraysOfStrings(const char** a, uint32_t num_a, const char** b, uint32_t num_b)
+{
+	std::vector<const char*> merge(a, a + num_a);
+	merge.insert(merge.end(), b, b + num_b);
+	return merge;
+}
+
+uint32_t selectQueueFamilyIndex(VkPhysicalDevice physical_device, VkSurfaceKHR surface) {
+	// Get the number of different queue families for the given physical device:
+	uint32_t queue_family_count = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, nullptr);
+
+	// Get the queue families' data:
+	std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, queue_families.data());
+	
+	// TODO: Find a suitable queue family index and return it!
+	
+	VKL_EXIT_WITH_ERROR("Unable to find a suitable queue family that supports graphics and presentation on the same queue.");
+}
